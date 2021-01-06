002af75cb00a699fec5445b9fd8d936f1e6c3120
HyojunK <gywns0608@naver.com>
2021-01-06 19:45:51 +0900

    백준 알고리즘 풀이

---
+++ Baekjoon/3053_택시 기하학.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double r = scan.nextDouble();
+		
+		System.out.println(r*r*Math.PI);
+		System.out.println(r*r*2);
+		
+	}
+}//class end
