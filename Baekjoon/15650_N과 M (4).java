66c15e9498382d034ccaf3d13e66e72dac2e50a9
HyojunK <gywns0608@naver.com>
2021-02-02 13:39:05 +0900

    백준 알고리즘 풀이

---
+++ Baekjoon/15650_N과 M (4).java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int[] arr;
+	public static int n, m;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+		dfs(n, m, 1, 0);
+		System.out.println(sb);
+		
+		
+	}
+	
+	//Depth First Search
+	public static void dfs(int n, int m, int at, int depth) {
+		//깊이가 m과 같으면 배열을 출력
+		if(depth == m) {
+			for(int i : arr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = at; i <= n; i++) {
+
+			arr[depth] = i;
+			dfs(n, m, i, depth + 1);
+		}
+	}
+}//class end
