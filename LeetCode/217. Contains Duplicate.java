a4384320d259ee48f8d60778e4996eb4e1b7964d
Hyojun Kim <gywns0608@naver.com>
2022-09-19 22:42:34 +0900

    Update 217. Contains Duplicate.java

--- LeetCode/217. Contains Duplicate.java
+++ LeetCode/217. Contains Duplicate.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Boolean> dataMap = new HashMap<>(); // 탐색한 데이터를 저장할 Map
-		for(int i : nums) {
-			if(dataMap.containsKey(i)) { // 이미 탐색한 값인 경우 (중복이 존재)
-				return true;
-			} else { // 처음 탐색한 값인 경우
-				dataMap.put(i, true); // 현재 값을 탐색한 데이터 Map에 저장				
-			}
-		}
-		
-		return false; // 배열을 모두 탐색 후 중복 값이 없으면 false를 return
+	for(int i : nums) {
+	    if(dataMap.containsKey(i)) { // 이미 탐색한 값인 경우 (중복이 존재)
+	        return true;
+	    } else { // 처음 탐색한 값인 경우
+	        dataMap.put(i, true); // 현재 값을 탐색한 데이터 Map에 저장
+	    }
+        }
+        return false; // 배열을 모두 탐색 후 중복 값이 없으면 false를 return
     }
 }
