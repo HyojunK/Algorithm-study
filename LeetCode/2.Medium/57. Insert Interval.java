014d5492204c5b8d4cd1f60f8003d40a45d0ae65
HyojunK <gywns0608@naver.com>
2023-01-09 23:29:42 +0900

    LeetCode 알고리즘 풀이

--- LeetCode/2.Medium/57. Insert Interval.java
+++ LeetCode/2.Medium/57. Insert Interval.java
@@ -4,19 +4,19 @@ class Solution {
         List<int[]> resultList = new ArrayList<>();
         // 모든 인터벌 탐색
         for(int[] interval : intervals) {
-            // 합칠 대상 인터벌의 시작점이 탐색중인 인터벌의 종료점보다 큰 경우
+            // 합칠 대상 인터벌이 탐색중인 인터벌보다 뒤에 있는 경우
             if(newInterval[0] > interval[1]) {
                 // 현재 탐색중인 인터벌을 리스트에 추가
                 resultList.add(interval);
             }
-            // 탐색중인 인터벌의 시작점이 합칠 대상 인터벌의 종료점보다 큰 경우
+            // 합칠 대상 인터벌이 탐색중인 인터벌보다 앞에 있는 경우
             else if(interval[0] > newInterval[1]){
                 // 합칠 대상 인터벌을 리스트에 추가
                 resultList.add(newInterval);
                 // 합칠 대상 인터벌을 현재 탐색 인터벌로 갱신
                 newInterval = interval;
             }
-            // 현재 탐색중인 인터벌과 합칠 대상 인터벌이 겹치는 경우
+            // 합칠 대상 인터벌과 탐색중인 인터벌이 겹치는 경우 
             else {
                 // 합칠 대상 인터벌의 시작점과 종료점을 각각 최소, 최대값으로 갱신
                 newInterval[0] = Math.min(interval[0], newInterval[0]);
