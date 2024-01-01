7efdf9e375bfc84a052c32c553fefedc4cffa8db
HyojunK <gywns0608@naver.com>
2024-01-01 23:03:00 +0900

    LeetCode 알고리즘 풀이

---
+++ LeetCode/1.Easy/455. Assign Cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int cookieIndex = 0;
+        int childIndex = 0;
+
+        while(cookieIndex < s.length && childIndex < g.length) {
+            if(g[childIndex] <= s[cookieIndex]) {
+                childIndex++;
+            }
+            cookieIndex++;
+        }
+
+        return childIndex;
+    }
+}
