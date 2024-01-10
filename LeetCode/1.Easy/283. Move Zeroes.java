0c7a390f53f773bcb14f2cb4fa7cb1feaa9f160d
HyojunK <gywns0608@naver.com>
2024-01-10 14:06:04 +0900

    LeetCode 알고리즘 풀이

---
+++ LeetCode/1.Easy/283. Move Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int curIndex = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[curIndex++] = nums[i];
+            }
+        }
+        for(int i = curIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
