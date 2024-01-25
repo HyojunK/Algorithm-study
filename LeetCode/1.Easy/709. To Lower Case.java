2b454cb47bbb2b93a5bd9ffb30601c25136fd8b5
Hyojun Kim <gywns0608@naver.com>
2024-01-25 16:40:43 +0900

    LeetCode 알고리즘 풀이

---
+++ LeetCode/1.Easy/709. To Lower Case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()) {
+            if(c > 64 && c < 91) {
+                c += 32;
+            }
+
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
